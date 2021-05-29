0a0e3db3e5bbf459b8452aa9dd27527a3d560c2a
sruba mohapatra <sruba209239@gmail.com>
2021-05-29 17:37:27 +0530

    added validator library

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const validator = require('validator');
 const notes = require("./notes.js");
 
 console.log(notes());
+
+console.log(validator.isEmail('@xyz@email.com'));
+console.log(validator.isURL('http/email.com'));
